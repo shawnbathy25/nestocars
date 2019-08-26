1e35e16b603c7b0c12548d5802d989472cd9851f
Oluwaseun Oyogho <shawnbathy25@example.com>
2019-08-26 16:26:26 +0000

    bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,11 +11,11 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require bootstrap.bundle.min
 //= require activestorage
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require popper
 //= require tether
-//= require bootstrap-sprockets
 //= require_tree .
