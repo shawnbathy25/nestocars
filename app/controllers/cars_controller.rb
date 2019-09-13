730b85ad0b29c82d1d069a0a6dfad524fa782538
Oluwaseun Oyogho <shawnbathy25@example.com>
2019-09-13 20:39:41 +0000

    handle form submission

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -6,4 +6,19 @@ class CarsController < ApplicationController
 	def new
 		@cars = Car.new
 	end
+
+
+	   def create
+	     Car.create(car_params)
+	     redirect_to root_path
+	   end
+
+	 private
+
+	 def car_params
+	   params.require(:car).permit(:make, :model, :price, :description)
+	 end
+
 end
+
+
